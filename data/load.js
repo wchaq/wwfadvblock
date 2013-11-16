a5cb0b60c11c1e7dff71d48e30566dfd7961a3df
Ahmad Fauzan <azon04@gmail.com>
2013-11-16 22:34:10 +0700

    Change Method to Load js

--- data/load.js
+++ data/load.js
@@ -1,14 +1,12 @@
-var elements = document.getElementsByTagName("a");
-var listAds;
-
-self.port.on("load_storage", function (valueStorage) {
-	listAds = valueStorage;
-});
-
-for (var i=0; i < elements.length; i++) {
-	var link = elements[i].href;
-	console.log(link);
-	if(link.indexOf("googleads") !== -1 || link.indexOf("adclick") !== -1) {
-		elements[i].style.visibility="hidden";
+self.port.on("loadPage", function(value) {
+	var elements = document.getElementsByTagName("a");
+	var listAds = value;
+	
+	for (var i=0; i < elements.length; i++) {
+		var link = elements[i].href;
+		console.log(link);
+		if(link.indexOf("googleads") !== -1 || link.indexOf("adclick") !== -1) {
+			elements[i].style.visibility="hidden";
+		}
 	}
-}
+});
