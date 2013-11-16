d74978a0d133abdabe7441bd2d5f502fb3cd7fe7
Muhammad Wachid Kusuma <wachidx@gmail.com>
2013-11-16 23:02:37 +0700

    comparison of current link and link from database

--- data/load.js
+++ data/load.js
@@ -4,9 +4,12 @@ self.port.on("loadPage", function(value) {
 	
 	for (var i=0; i < elements.length; i++) {
 		var link = elements[i].href;
-		console.log(link);
-		if(link.indexOf("googleads") !== -1 || link.indexOf("adclick") !== -1) {
-			elements[i].style.visibility="hidden";
+		//console.log(link);
+		for (var j=0; j < listAds.length; j++){
+			console.log(listAds[j]);
+			if(link.indexOf(listAds[j]) !== -1){
+				elements[i].style.visibility="hidden";
+			}
 		}
 	}
 });
