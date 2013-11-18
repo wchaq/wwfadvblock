c9392c5035e1581077498edc7eec64a7760c8867
wchaq <chaqjozz@rocketmail.com>
2013-11-18 16:37:00 +0700

    finalisasi

--- data/get-text.js
+++ data/get-text.js
@@ -9,7 +9,11 @@ var listItemView = document.getElementById("list_filter");
 //
 // Set the focus to the text area so the user can
 // just start typing.
-self.port.on("show", function onShow(storage) {
+self.port.on("show", function onShow(storage,toogle) {
+	if(toogle == 1)
+		tl.checked = true;
+	else if(toogle == 0)
+		tl.checked = false;
 	var i;
 	listItemView.innerHTML = "";
 	for(i = 0; i < storage.length;i++){
@@ -38,6 +42,15 @@ function advFunc() {
   self.port.emit("add-adv",document.getElementById("text-input").value);
 }
 
-// add event listener to t
+// add event listener to button
 var el = document.getElementById("btnAdv");
 el.addEventListener("click", advFunc, false);
+
+// Function to change the content of t2
+function toogleFunc() {
+  self.port.emit("toogle");
+}
+
+// add event listener to button
+var tl = document.getElementById("toogle");
+tl.addEventListener("click", toogleFunc, false);
